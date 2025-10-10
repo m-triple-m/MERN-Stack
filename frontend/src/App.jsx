6efecaed9a84a4014f6009bc54f7cd5fbc382b5e
m-triple-m <triplem656@gmail.com>
2025-10-10 21:16:03 +0530

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import './App.css';
 import MyRoutes from './routes';
 import { Link, BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
       <Router>
-        <div className='flex gap-4'>
-          <Link className='text-sky-700 underline' to="/">Home</Link>
-          <Link className='text-sky-700 underline' to="/signup">Signup</Link>
-          <Link className='text-sky-700 underline' to="/login">Login</Link>
-        </div>
+        <Navbar />
         <MyRoutes />
       </Router>
     </div >
